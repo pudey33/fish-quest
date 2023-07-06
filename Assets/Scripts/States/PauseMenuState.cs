15f42b82f524e33ab518d2b7543b3d7d4ffe9352
TJ Brosnan <taylor.brosnan@gmail.com>
2023-07-05 22:20:07 -0600

    More States!

---
+++ Assets/Scripts/States/PauseMenuState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : State
+{
+    public override void EnterState(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+        // Insert code to handle entering the main menu state here
+    }
+
+    public override void Update()
+    {
+        // Insert code to handle updates in the main menu state here
+    }
+
+    public override void ExitState()
+    {
+        // Insert code to handle exiting the main menu state here
+    }
+}
