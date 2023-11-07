78e4d4f90d82d475dd4fa4bdb0c24d142dd0e57e
TJ Brosnan <taylor.brosnan@gmail.com>
2023-11-06 19:36:53 -0700

    Changed some fields on Fish to be more usable.

--- Assets/Scripts/Core/Entities/Fish.cs
+++ Assets/Scripts/Core/Entities/Fish.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
     public class Fish
     {
         public string name { get; set; }
-        public int depthRange { get; set; }
+        public int depthMax { get; set; }
+        public int depthMin { get; set; }
         public int rarity { get; set; }
-        public int moneyRange { get; set; }
+        public int baseMoney { get; set; }        public int starsMax { get; set; }
         public int starLevel { get; set; }
-        public int lengthRange { get; set; }
+        public int lengthMax { get; set; }
+        public int lengthMin { get; set; }
         public int lureChance { get; set; }
         public int lureLevel { get; set; }
     }
